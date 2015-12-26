245c7981af3846a2df62cf382c8bb5bf965258a1
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2015-12-26 19:29:10 +0100

    Edited filters

--- install.js
+++ install.js
@@ -2,11 +2,13 @@
 
 module.exports = function(done) {
   var fs = require('fs');
+  var appDir = alquimia.getPath('appDir');
+  var scriptsDir = alquimia.getPath('scriptsDir');
+  var stylesDir = alquimia.getPath('stylesDir');
 
-  fs.mkdirSync('app/src/slideshow');
-  alquimia.copy(__dirname + '/assets/slideshow/src', 'app/src/slideshow');
-
-  alquimia.copy(__dirname + '/assets/slideshow/scss', 'app/scss/components');
+  fs.mkdirSync(appDir + '/' + scriptsDir + '/slideshow');
+  alquimia.copy(__dirname + '/assets/slideshow/src', appDir + '/' + scriptsDir + '/slideshow');
+  alquimia.copy(__dirname + '/assets/slideshow/scss', appDir + '/' + stylesDir + '/components');
 
   done();
 };
--- uninstall.js
+++ uninstall.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = function(done) {
-  alquimia.del('app/src/slideshow');
+  alquimia.del(alquimia.getPath('appDir') + '/' + alquimia.getPath('scriptsDir') + '/slideshow');
+  alquimia.del(alquimia.getPath('appDir') + '/' + alquimia.getPath('stylesDir') + '/components/_slideshow.scss');
   done();
 };
